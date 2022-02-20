68b6a0541527c1dfeae7ed5474c937d7443b73a7
JavierCorralLizarraga <javiercorraljr@hotmail.com>
2022-02-19 20:03:48 -0600

    extraccion y rename de csv

--- code/proyecto_1.sh
+++ code/proyecto_1.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+# estaciones.xml existe
+if [[ ! -f estaciones.xml ]] # -f exists and is a file  
+then
+    wget https://bit.ly/2V1Z3sm
+    mv 2V1Z3sm estaciones.xml # rename
+fi
+
+# estaciones.xml existe
+if [[ ! -f precios.xml ]]
+then
+    wget https://bit.ly/2JNcTha
+    mv 2JNcTha precios.xml
+fi
+
+
+
+
+
+
