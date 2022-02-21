dfaccfedefae069fce3a3cb98b0f6ac66bb78f1d
JavierCorralLizarraga <javiercorraljr@hotmail.com>
2022-02-21 12:30:39 -0600

    add PreguntasPrecios file

---
+++ code/PreguntasPrecios.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 1. Con grep averigua ¿Cuántas gasolineras sirven gasolina regular
+#    en este conjunto de datos? Imprime en terminal gasolineras que 
+#    sirven gasolina regular: <z> . Donde  <z>  corresponde al 
+#    número de gasolineras.
+
+# 2. Con grep averigua ¿Cuántas gasolineras sirven gasolina  diesel
+#    en este conjunto de datos? Imprime en terminal gasolineras que 
+#    sirven gasolina diesel: <y> . Donde  <y>  corresponde al número 
+#    de gasolineras.
+
+# 3. Con grep averigua ¿Cuántas gasolineras sirven  premium  en este
+#    conjunto de datos? Imprime en terminal  gasolineras que sirven 
+#    gasolina premium: <x> 
+
+# 4. Con AWK averigua ¿De cuántas gasolineras diferentes tienes datos 
+#    de precios? * Te puedes apoyar de un archivo auxiliar creado 
+#    con  grep  o  sed . Imprime en terminal  gasolineras diferentes:
+#    <m> . Donde  <m>  corresponde al número de gasolineras.
+
+# 5. ¿Cuántos renglones de precios de gasolina tienes (una vez que 
+#    ya tienes 1 renglón por estación de gasolina)? Imprime en 
+#    terminal  observaciones de precios: <n> . Donde  <n>  
+#    corresponde al número de gasolineras.
