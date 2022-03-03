3d26885282e9624e8ec02cee721cfeffd6f2e23d
JavierCorralLizarraga <javiercorraljr@hotmail.com>
2022-03-03 12:38:28 -0600

    transform 2 listo, estaciones.csv

--- code/proyecto_1_transform_2.sh
+++ code/proyecto_1_transform_2.sh
@@ -2,13 +2,13 @@
 
 #estaciones=$1
 estaciones=estaciones.xml
-columnas="estacion_servicio, regular, premium, diesel" 
+columnas="id_estacion,nombre,latitud,longitud" 
 
-touch precios.csv
-echo $columnas > precios.csv
+touch estaciones.csv
+echo $columnas > estaciones.csv
 touch aux
 touch BloqueEstacion
-grep -nP '(?<=place_id=")\d{4,5}(?=">)' $precios | awk -F ":" '!_[$2]++' | cut  -d : -f 1  > aux # return archivo con numeros de linea del archivo precios en la posicion del id y lidiamos con ids repetidos
+grep -nP '(?<=place_id=")\d{4,5}(?=">)' $estaciones | awk -F ":" '!_[$2]++' | cut  -d : -f 1  > aux # return archivo con numeros de linea del archivo precios en la posicion del id y lidiamos con ids repetidos
 line1="$(awk 'NR==1' aux)" 
 line2="$(awk 'NR==2' aux)"  
 cat aux | while read line # loopeamos linea a linea a traves de aux 
@@ -24,13 +24,13 @@ do
         pre=$post
         post=$line
     fi
-    awk -v pre=$pre -v post=$post 'NR>=pre && NR<=post' $precios  > BloqueEstacion # tomamos el bloque del archivo desde un id hasta el otro 
+    awk -v pre=$pre -v post=$post 'NR>=pre && NR<=post' $estaciones  > BloqueEstacion # tomamos el bloque del archivo desde un id hasta el otro 
     sed -i '$ d' BloqueEstacion # borramos el 2do id matcheado para que no haya confusiones
     id="$(grep -oP '(?<=place_id=")\d{4,5}(?=">)' BloqueEstacion | uniq)" # matcheamos el id del bloque
-    regular="$(grep -oP '(?<=regular">)\d+\.{0,1}\d*(?=<\/gas_price>)' BloqueEstacion)" # matcheamos el precio regular del bloque
-    premium="$(grep -oP '(?<=premium">)\d+\.{0,1}\d*(?=<\/gas_price>)' BloqueEstacion)" # matcheamos el precio premium del bloque
-    diesel="$(grep -oP '(?<=diesel">)\d+\.{0,1}\d*(?=<\/gas_price>)' BloqueEstacion)" # matcheamos el precio diesel del bloque
-    echo $id','$regular','$premium','$diesel >> precios.csv # agregamos los datos como una nueva linea a precios.csv
+    name="$(grep -oP '(?<=<name>).*(?=<\/name>)' BloqueEstacion)" # matcheamos el nombre del bloque
+    latitud="$(grep -oP '(?<=<x>).*(?=<\/x>)' BloqueEstacion)" # matcheamos la latitud del bloque
+    longitud="$(grep -oP '(?<=<y>).*(?=<\/y>)' BloqueEstacion)" # matcheamos la longitud del bloque
+    echo $id','$name','$latitud','$longitud >> estaciones.csv # agregamos los datos como una nueva linea a precios.csv
 done
 rm aux 
 rm BloqueEstacion
