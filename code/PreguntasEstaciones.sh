a69a403b74e43cfe72fb8738293623e4459a47d4
JavierCorralLizarraga <javiercorraljr@hotmail.com>
2022-03-03 18:27:00 -0600

    primer pregunta estaciones

--- code/PreguntasEstaciones.sh
+++ code/PreguntasEstaciones.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# recuerda el -i (inplace) en todas
+
 # 2. Con  sed  cambiar a minúsculas el contenido del atributo  name  (sin crear otro archivo)
-sed 's/"[A-Z]+"/\L&/g' estaciones.csv
+sed 's/[A-Z]/\L&/g' estaciones.csv
 
 # 3. Con  sed  elimina los acentos del contenido del atributo  name  (sin crear otro archivo)
-a
+
 
 # 4. Con  sed  elimina signos de puntuación del atributo  name  (sin crear otro archivo)
 
