68fcf9d3411659d8ee685bbb724057a498fd3ff4
JavierCorralLizarraga <javiercorraljr@hotmail.com>
2022-03-07 12:26:28 -0600

    le faltaba la tabla de estaciones al load proyecto 1

--- code/proyecto_1_load.sh
+++ code/proyecto_1_load.sh
@@ -3,3 +3,5 @@
 psql -f updload.sql service=fuentes_datos
 
 psql -c '\copy raw.precios from precios.csv with csv header'service=fuentes_datos 
+
+psql -c '\copy raw.estaciones from estaciones.csv with csv header;' service=fuentes_datos
