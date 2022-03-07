0f9e1dd294d9a8684344db91d1e49f6bfb07ddcc
JavierCorralLizarraga <javiercorraljr@hotmail.com>
2022-03-07 13:14:45 -0600

    robustecer creacion de tablas

--- code/upload.sql
+++ code/upload.sql
@@ -2,16 +2,16 @@ drop schema if exists raw cascade;
 create schema raw;
 drop table if exists raw.estaciones;
 create table raw.estaciones(
-    id_estacion numeric(6,0) not null unique, 
-    nombre varchar(100), 
-    latitud numeric(4,2), 
-    longitud numeric(4,2), 
+    id_estacion numeric(7,0) not null unique, 
+    nombre varchar(250), 
+    latitud numeric(5,2), 
+    longitud numeric(5,2), 
 );
 drop table if exists raw.precios;
 create table raw.precios(
-    estacion_servicio numeric(6,0) not null unique, 
-    regular numeric(4,2), 
-    premium numeric(4,2), 
-    diesel numeric(4,2), 
+    estacion_servicio numeric(7,0) not null unique, 
+    regular numeric(5,2), 
+    premium numeric(5,2), 
+    diesel numeric(5,2), 
 );
 
