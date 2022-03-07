0054057c85eed3c704ea99904e455425761669ab
JavierCorralLizarraga <javiercorraljr@hotmail.com>
2022-03-07 11:57:32 -0600

    add .pg_service.conf

--- code/listo/proyecto_1_transform_1.sh
+++ code/listo/proyecto_1_transform_1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 precios=$1 
-columnas="estacion_servicio, regular, premium, diesel" 
+columnas="estacion_servicio,regular,premium,diesel" 
 
 touch precios.csv
 echo $columnas > precios.csv
---
+++ code/proyecto_1_load.sh
@@ -0,0 +1,4 @@
+ psql -f updload.sql service=fuentes_datos
+
+psql -c '\copy raw.precios from precios.csv with csv header;'
+service=fuentes_datos 
